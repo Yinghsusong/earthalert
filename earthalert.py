8232f1bfe039a63927455d46a94f51075cd7e524
Michael House <blankstamped@gmail.com>
2017-04-29 13:08:10 -0500

    set up submission to the db

--- earthalert.py
+++ earthalert.py
@@ -29,11 +29,15 @@ def index():
 def report():
 	number =''
 	if request.method == "POST":
-		number = request.values.get('text')
+		number = request.values.get('number')
+		lat = request.values.get('lat', None)
+		lon = request.values.get('lon', None)
 		if number:
 			person = models.Person()
-			person.phone = number
-		return render_template( 'report.html', number=number)
+			person.set( lat, lon, number )
+			session.add( person )
+			session.commit()
+		return render_template( 'report.html' )
 	else:
 		return render_template( 'report.html')
 
