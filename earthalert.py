14f8f6c324cd365c1261a46f9f34c0d8f22f96c8
Michael House <blankstamped@gmail.com>
2017-04-29 10:34:44 -0500

    created basic templates

--- earthalert.py
+++ earthalert.py
@@ -1,18 +1,32 @@
-from package import models
-
+# ------------------------------------------------------------------------------
+# outside imports
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from flask import Flask
+
+# local imports
+from package import models
+
+# initial setup
 db_engine = create_engine( 'earthalert.db' )
 session_generator = sessionmaker(bind=db_engine)
 session = session_generator()
 
-from flask import Flask
+# init the app
 app = Flask(__name__)
 
+# this is the index page. Going to http://localhost:5000/ when the
+# project is running will bring you here
 @app.route("/")
 def index():
 	events = session.query( models.Event ).all()
 	return render_template( 'index.html', events=events )
+
+# this is the index page. Going to http://localhost:5000/ when the
+# project is running will bring you here
+@app.route("/report")
+def report():
+	return render_template( 'report.html')
 
 if __name__ == "__main__":
     app.run()
--- package/models.py
+++ package/models.py
@@ -1,7 +1,6 @@
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
 
@@ -10,37 +9,9 @@ class Event(Base):
 	id = Column(Integer, primary_key=True)
 	date = Column(String(250))
 	time = Column(String(250))
-	country = Column(String(250))
-	nearest_places = Column(String(250))
-	hazard_type = Column(String(250))
-	landslide_type = Column(String(250))
-	trigger = Column(String(250))
-	storm_name = Column(String(250))
-	fatalities = Column(String(250))
-	injuries = Column(String(250))
-	source_name = Column(String(250))
-	source_link = Column(String(250))
-	location_description = Column(String(250))
-	location_accuracy = Column(String(250))
-	landslide_size = Column(String(250))
-	photos_link = Column(String(250))
-	cat_src = Column(String(250))
-	cat_id = Column(String(250))
-	countryname = Column(String(250))
-	near = Column(String(250))
-	distance = Column(String(250))
-	adminname1 = Column(String(250))
-	adminname2 = Column(String(250))
-	population = Column(String(250))
-	countrycode = Column(String(250))
-	continentcode = Column(String(250))
-	key = Column(String(250))
-	version = Column(String(250))
-	tstamp = Column(String(250))
-	changeset_id = Column(String(250))
+	location = Column(String(250))
 	latitude = Column(String(250))
 	longitude = Column(String(250))
-	geolocation = Column(String(250))
 
 	def __init__( self, *args, **kwargs ):
 		pass
@@ -49,17 +20,6 @@ class Event(Base):
 		for key, value in kwargs.items():
 			setattr(self,key,value)
 
-def dump( dataset ):
-	events = []
-	with open( dataset, 'r', newline='' ) as _file:
-		headings = _file.readline().split(',')
-		csv = csvx.csv.Reader(_file)
-		for row in csv:
-			data = dict(zip(headings,row))
-			event = Event()
-			event.set( **data )
-			events.append( event )
-	return events
-
 if __name__=='__main__':
-	pass
+	db_engine = create_engine( 'earthalert.db' )
+	Base.metadata.create_all(db_engine)
