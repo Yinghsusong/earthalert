a5bd7a59f182ddd66b7152bcb64da04772505643
Will Koehrsen <wjk68@case.edu>
2017-04-29 18:51:19 -0600

    Added marker for location

--- static/js/earthalert.js
+++ static/js/earthalert.js
@@ -18,6 +18,10 @@ function update( position ){
 		window.lat = position.coords.latitude;
 		window.lon = position.coords.longitude;
 		var center = new google.maps.LatLng(window.lat, window.lon)
+		var marker = new google.maps.Marker({
+          position: {lat: window.lat, lng:window.lon},
+          map: map
+        });
 		map.panTo( center );
 		map.setZoom(10);
 	} else {
