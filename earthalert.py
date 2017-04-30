6be85518a3f19d0795e0bad31f2904f915a71709
Michael House <blankstamped@gmail.com>
2017-04-30 12:06:40 -0500

    update

--- earthalert.py
+++ earthalert.py
@@ -136,7 +136,6 @@ def sms_reply():
 
 		response = messaging_response.MessagingResponse()
 		response.message('Your risk level is: {} ({})'.format(level,danger_level),to=number,from_='2563611265')
-		log.write( response.to_xml() )
 		return response.to_xml()
 	except Exception as e:
 		log.write(str(type(e)) + ': ' + str(e) + '\n')
